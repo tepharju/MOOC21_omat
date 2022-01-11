89a9a5674fcf1ea610d4de4f896f388d211db2a9
Teppo Harju <36477693+tepharju@users.noreply.github.com>
2022-01-11 10:47:16 +0200

    Add files via upload

---
+++ osa11-16_suurin_alkio/src/suurin_alkio.py
@@ -0,0 +1,7 @@
+# TEE RATKAISUSI TÄHÄN:
+class Alkio:
+    """ Luokka mallintaa yhtä alkiota binääripuussa """
+    def __init__(self, arvo, vasen_lapsi:'Alkio' = None, oikea_lapsi:'Alkio' = None):
+        self.arvo = arvo
+        self.vasen_lapsi = vasen_lapsi
+        self.oikea_lapsi = oikea_lapsi
---
+++ osa14-01_pelin_palautus/src/main.py
@@ -0,0 +1,1 @@
+# TEE PELI TÄHÄN
